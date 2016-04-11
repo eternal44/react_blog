e8bc8979012362f6ef0dabf81bcb85f9370494e8
James Youn <jamesyoun710@gmail.com>
2016-04-11 16:18:47 -0700

    STEP 1 - Scaffold beginning for posts rendering

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>List of blog posts</div>;
+// removing this import in favor of using objects
+// import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+  // lifecycle method - automatically called by react whenever this DOM is rendered for the first time
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// using an object instead of this function below to export
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+                              // using { fetchPosts } instead of { fetchPosts: fetchPosts }
+export default connect(null, { fetchPosts })( PostsIndex );
