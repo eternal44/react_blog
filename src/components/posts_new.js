cce08270ff2f02713d51291381568fd30a7f0ed6
James Youn <jamesyoun710@gmail.com>
2016-04-12 16:09:01 -0700

    Redirect user to index page on successful post creation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+
+  // object below defines a contextTypes object on the 'PostsNew' object class
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => { 
+        // blog post has been created, navigate the user to the index
+        // we navigate by calling this.context.router.push with the
+        // new path to navigate to
+        this.context.router.push('/');
+      })
+
+  }
+
   render() {
     // const title = this.props.fields.title
     // the line below is the line above in es6
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create A New Post</h3>
 
         {/* using interpolation to conditionally assign CSS class*/}
