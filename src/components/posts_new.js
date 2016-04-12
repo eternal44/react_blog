7dbf941093f20e5a4b6e73b889073681915485f2
James Youn <jamesyoun710@gmail.com>
2016-04-12 15:42:36 -0700

    Add form validation for new posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,26 +12,54 @@ class PostsNew extends Component {
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
 
-        <div className="form-group">
+        {/* using interpolation to conditionally assign CSS class*/}
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           {/* '{...title}' destructures title object and passes into input */}
           <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : null}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : null}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <input type="textarea" className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : null}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+
+  if(!values.categories) {
+    errors.categories = "Enter categories"
+  }
+
+  if(!values.content) {
+    errors.content = "Write something"
+  }
+
+  return errors;
 }
 
 // connect: 1st argument is mapStateToProps
@@ -44,5 +72,6 @@ class PostsNew extends Component {
 export default reduxForm({
   // form name has to be unique
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
